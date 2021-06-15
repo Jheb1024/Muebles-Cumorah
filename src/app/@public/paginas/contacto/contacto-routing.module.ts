ea2994179d63e2b6cbb8127d326bab63301e87be
John <jhebreak@gmail.com>
2021-06-15 01:32:57 -0500

    Creacipon de rutas publica

--- src/app/@public/paginas/contacto/contacto-routing.module.ts
+++ src/app/@public/paginas/contacto/contacto-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component:ContactoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './@public/paginas/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: ()=>import("./@public/paginas/home/home.module").then(m=>m.HomeModule)
+  },
+  {
+    path:'contacto',
+    loadChildren:()=>import("./@public/paginas/contacto/contacto.module").then(m=>m.ContactoModule)
+  },
+  {
+    path: '',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path: '**',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {useHash:true,
+    scrollPositionRestoration:'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
